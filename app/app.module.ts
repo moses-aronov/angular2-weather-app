5700b9edb5f5d7df88abeb27724bc1924dab1508
Moses Aronov <moses@aronov.me>
2017-03-07 12:35:08 -0500

    Create app module

---
+++ app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+//Allows you to run the application in a browser
+import { BrowserModule } from '@angular/platform-browser';
+
+//Create a decorator
+//A decorator modifies an instance before it will be used.
+@NgModule({
+    imports : [BrowserModule]
+})
+
+export class AppModule{}
