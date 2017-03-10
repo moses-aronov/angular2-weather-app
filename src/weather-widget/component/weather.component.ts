3a85b9c7f4b8b3feda5a13dcf1f5dc9e182c3e33
Moses Aronov <moses@aronov.me>
2017-03-10 10:55:09 -0500

    Added init function and copied weather dta to weatherData

--- src/weather-widget/component/weather.component.ts
+++ src/weather-widget/component/weather.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { WeatherService } from '../service/weather.service';
 import { Weather } from '../model/weather'
 
@@ -10,19 +10,39 @@ import { Weather } from '../model/weather'
     providers: [WeatherService]
 })
 
-export class WeatherComponent {
+export class WeatherComponent implements OnInit {
     pos: Position;
-    WeatherData = new Weather(null, null, null, null, null)
-    
-    constructor(private service: WeatherService) {
-        //Save position data
+    weatherData = new Weather(null, null, null, null, null)
+
+    constructor(private service: WeatherService) { }
+
+    //Hook into the initilization of component
+    ngOnInit() {
+        this.getWeatherForCurrentLocation()
+    }
+
+    getWeatherForCurrentLocation() {
         this.service.getCurrentLocation()
             .subscribe(position => {
                 this.pos = position
-                this.service.getCurrentWeather(this.pos.coords.latitude, this.pos.coords.longitude)
-                    .subscribe(weather => console.log(weather),
-                    err => console.error(err))
+                this.getCurrentWeather()
             },
             err => console.log(err))
+    }
+
+    getCurrentWeather() {
+        this.service.getCurrentWeather(this.pos.coords.latitude, this.pos.coords.longitude)
+            .subscribe(weather => {
+                //Copy data from results
+                this.weatherData.humidity = weather["currently"]["humidity"],
+                this.weatherData.icon = weather["currently"]["icon"],
+                this.weatherData.summary = weather["currently"]["summary"]
+                this.weatherData.temp = weather["currently"]["temperature"],
+                this.weatherData.wind = weather["currently"]["windSpeed"],                              
+                this.weatherData.summary = weather["currently"]["summary"]
+                console.log("Weather: ", this.weatherData)
+
+            },
+            err => console.error(err))
     }
 }
