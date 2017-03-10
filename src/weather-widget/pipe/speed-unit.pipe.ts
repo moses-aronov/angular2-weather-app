9587e8c225b26a1614ee65339facf99be13408a3
Moses Aronov <moses@aronov.me>
2017-03-10 13:28:24 -0500

    Modified speed units to improve formatting

--- src/weather-widget/pipe/speed-unit.pipe.ts
+++ src/weather-widget/pipe/speed-unit.pipe.ts
@@ -7,12 +7,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SpeedUnitPipe implements PipeTransform {
     transform(speed: number, unitType: string) {
         switch (unitType) {
-            case 'mph':
-                const miles = speed * 1.6;
-                return miles + unitType
+            case 'kph':
+                return this.formatSpeed(this.convertMPHtoKPH(speed)) + unitType
             default:
-                const kilometers = speed;
-                return kilometers + unitType;
+                return this.formatSpeed(speed) + unitType
         }
+    }
+
+    convertMPHtoKPH(speed){
+        return speed*1.60934
+    }
+
+    formatSpeed(speed: number){
+        return parseInt(String(speed), 10)
     }
 }
